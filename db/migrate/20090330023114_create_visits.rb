6105bacfec4a35bcd9defc62427d22070df87a9a
lecter255 <lecter255@126f7d6c-fee3-11dd-88c7-29a3b14d5316>
2009-04-01 15:54:42 +0000

    committing migration files for allergies, vaccinations, and visits
    git-svn-id: https://matlabulchifai.googlecode.com/svn/trunk@46 126f7d6c-fee3-11dd-88c7-29a3b14d5316

---
+++ db/migrate/20090330023114_create_visits.rb
@@ -0,0 +1,16 @@
+class CreateVisits < ActiveRecord::Migration
+  def self.up
+    create_table :visits do |t|
+      t.string :reason
+      t.date :dateSeen
+      t.string :followUp
+      t.string :note
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :visits
+  end
+end
