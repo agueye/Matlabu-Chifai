dbd82d3e9186562481734204dce929d66afb16c2
lecter255 <lecter255@126f7d6c-fee3-11dd-88c7-29a3b14d5316>
2009-04-20 05:29:45 +0000

    sanitized input in alert and patient.rb
    git-svn-id: https://matlabulchifai.googlecode.com/svn/trunk@88 126f7d6c-fee3-11dd-88c7-29a3b14d5316

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,3 +1,4 @@
 class Alert < ActiveRecord::Base
+	attr_accessible :patient_id, :alert_date, :alert_note, :active
 	belongs_to :patient
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -8,7 +8,7 @@ class Patient < ActiveRecord::Base
   #old
   def self.old_search(query)
     unless query.to_s.strip.empty?
-       find(:all, :conditions => ['first_name like ? or middle_name like ? or last_name like ?', "%#{query}%", "%#{query}%", "%#{query}%"], :order => "last_name desc", :limit => 25)
+       find(:all, :conditions => ['first_name like ? or middle_name like ? or last_name like ?', query, query, query], :order => "last_name desc", :limit => 25)
     else
       []
     end
