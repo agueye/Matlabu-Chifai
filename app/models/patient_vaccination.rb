977a81a55127c670a5cb2cfe733a52aa013b6a16
v.shyam121 <v.shyam121@126f7d6c-fee3-11dd-88c7-29a3b14d5316>
2009-05-11 09:01:59 +0000

    patient_vaccinations encryption
    git-svn-id: https://matlabulchifai.googlecode.com/svn/trunk@262 126f7d6c-fee3-11dd-88c7-29a3b14d5316

--- app/models/patient_vaccination.rb
+++ app/models/patient_vaccination.rb
@@ -1,10 +1,10 @@
 class PatientVaccination < ActiveRecord::Base
   belongs_to :patient
   belongs_to :vaccination
-  
-  validates_presence_of :patient
+  encrypt :notes
+  #validates_presence_of :patient
   validates_presence_of :vaccination
-  validates_presence_of :date_admined
+  #validates_presence_of :date_admined
   
   validates_length_of   :notes, :maximum => 10000, :allow_blank => true  
   
