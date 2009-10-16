32fca4b936010f9c1ff056f01262b9bbe7dfad60
bowei.du <bowei.du@126f7d6c-fee3-11dd-88c7-29a3b14d5316>
2009-10-16 23:48:27 +0000

    fixed migration schema.rb, which does not take into account the creation of the superuser account
    git-svn-id: https://matlabulchifai.googlecode.com/svn/trunk@299 126f7d6c-fee3-11dd-88c7-29a3b14d5316

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090510220305) do
+ActiveRecord::Schema.define(:version => 20090427032545) do
 
   create_table "allergies", :force => true do |t|
     t.string   "name"
