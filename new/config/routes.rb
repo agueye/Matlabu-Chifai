88bfde4ba88f5eb3c1418a3cf1b37c3f7dc01ad7
amber.feng <amber.feng@126f7d6c-fee3-11dd-88c7-29a3b14d5316>
2009-11-13 23:05:28 +0000

    git-svn-id: https://matlabulchifai.googlecode.com/svn/trunk@309 126f7d6c-fee3-11dd-88c7-29a3b14d5316

--- new/config/routes.rb
+++ new/config/routes.rb
@@ -1,4 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'users', :action => 'create'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.resources :users
+
+  map.resource :session
+
   map.resources :patients
 
   map.resources :visit_types
@@ -58,6 +66,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
+  map.root :controller => "dashboard"
 
   # See how all your routes lay out with "rake routes"
 
