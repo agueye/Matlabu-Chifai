d8a98cbcab6a88af969f47c4470a63dffb3f4a89
Brian Maissy <brian.maissy@gmail.com>
2011-10-16 14:18:24 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MatlabuChifai::Application.config.secret_token = '13e3fd8a3d8c905202f7a6fd29d8c220344176db3d2927c8a77b66d99fe1678e8357b1aa05cab5087b328d792ea7d86a8a58ec7e87db8ec09b984cc3cfbeb0a3'
