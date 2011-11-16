81890ad60cb723932c2b3d0547eccec490ea5be8
Robert Shane <rshane00@gmail.com>
2011-11-16 14:55:38 -0800

    Fixed most of the errors need to still fix allergen with side_effect

--- app/models/allergen.rb
+++ app/models/allergen.rb
@@ -3,12 +3,16 @@ class Allergen < ActiveRecord::Base
   validates_presence_of :name
   validate :val_side_effect
 
-  def val_side_effect
-   if :type == "Medication"
-      self[:side_effect]
-   else
-      errors.add(:type, "is not a Medication, so it should not have side effects")
-   end
+  def val_side_effect 
+#:type == "Medication" 
+#   if :type == "Medication"
+#      self[:side_effect]
+#   else
+      errors[:base] << "Type is not a Medication and should not access
+                        side_effect" unless side_effect == nil
+#      errors.add(:type, side_effect)
+#"is not a Medication and should not access side_effect"
+#   end
   end 
 
 
